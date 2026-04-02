208916a386501d662f713f7a7b9197d3c834ada9
Manu Alvarez <manuelalvarezdianez@hotmail.com>
2026-04-02 12:45:19 +0200

    feat(livekit-nikolina): base path, crossorigin removed, deployed all 3 envs

--- apps/livekit-nikolina/frontend/vite.config.js
+++ apps/livekit-nikolina/frontend/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: './',
     server: {
-        host: true, // Escucha en TODAS las interfaces (0.0.0.0) para la red local
-        allowedHosts: true, // Permitir conexiones desde cloudflared
+        host: true,
+        port: 5174,
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:8081',
                 changeOrigin: true,
             }
         }
+    },
+    preview: {
+        port: 5174,
     }
 })
