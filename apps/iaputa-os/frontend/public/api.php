189c14ed83b55b3eec5fec8b0b692c44d1981c86
Manu Alvarez <manuelalvarezdianez@hotmail.com>
2026-04-25 13:59:40 +0200

    Integrar Web Restaurante Atenea al ecosistema y solucionar warnings PHP

--- apps/iaputa-os/frontend/public/api.php
+++ apps/iaputa-os/frontend/public/api.php
@@ -61,7 +61,7 @@ function call_gemini($system_prompt, $user_text) {
     
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
+
     
     if ($httpCode !== 200) {
         return null;
