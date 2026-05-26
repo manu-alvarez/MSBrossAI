0b4d8a9e0579f33c99cf87baacd8531a28546316
Manu Alvarez <manuelalvarezdianez@hotmail.com>
2026-05-26 13:16:32 +0200

    fix: añadido basePath a nextConfigs de teringo y jartosdto, recompilado y copiado a www/app, y configurado script de visitas con fallback resiliente en index.html

--- apps/jartosdto/client/next.config.ts
+++ apps/jartosdto/client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   output: "export",
+  basePath: "/app/jartosdto",
   images: {
     unoptimized: true,
   },
