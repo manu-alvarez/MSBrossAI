24410514ffc99735fdfbdcef561f4ec1268ef020
Manu Alvarez <manuelalvarezdianez@hotmail.com>
2026-06-23 23:54:23 +0200

    chore(root): remove leftover debug scripts and trash files from root directory

--- check_hash.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-demo_hash = b"$2b$12$z70N0KVRZgNaDNgK0KRr3ez9fu0RgbDfhQJGdeqkORNpHOXYx0/vC"
-test_hash = b"$2b$12$BfA.M3fiK4ri0nRW7t5veekT04qOvYpyg.5i9kxPA2eQUBy0mJFIm"
-
-print("Demo 1234:", bcrypt.checkpw(b"1234", demo_hash))
-print("Test 0000:", bcrypt.checkpw(b"0000", test_hash))
--- finish_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Waiting for copy tasks to finish..."
-sleep 10
-cd apps/txa-fitness-pro && npm run build
-cd ../mapfre-infocol/frontend && npm run build
-cd ../../../
-pm2 restart all
-pm2 save
-echo "DONE"
